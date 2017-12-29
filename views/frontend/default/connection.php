1f4cb4cf6e4a0086addd10afbab6fd8cef2b1a2c
donjale <bpsuki@gmail.com>
2017-12-29 06:00:12 +0300

    initialize

---
+++ views/frontend/default/connection.php
@@ -0,0 +1,7 @@
+<?php
+$servername='localhost';
+$username='root';
+$password='';
+$dbname='kouosl';
+$con=mysqli_connect($servername,$username,$password,$dbname);
+?>
--- views/frontend/default/index.php
+++ views/frontend/default/index.php
@@ -1,15 +1,78 @@
-<?php
-/* @var $this yii\web\View */
-$this->title = 'WYP Sample Application';
-?>
-<div class="site-index">
+<!DOCTYPE html>
+<html>
+<head>
+<title>Slideshow in bootstrap</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-    <div class="jumbotron">
-        <h1>Kou Osl Yii2 App - Frontend</h1>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-        <p class="lead">Örnek uygulamayı başarılı bir şekilde çalıştırdınız.</p>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-        <p><a class="btn btn-lg btn-success" href="#">Modüller ve konfürgasyon!</a></p>
-    </div>
+<style type="text/css">
+.carousel-inner>.img>img,
+.carousel-inner>.item>a>img{
+	width:100%;
+	margin:auto;
+}
+		<div class="container">
+				<div class="col-sm-9">
+						<div class="mycarousel" class="carousel slide" data-ride="carousel">
+							<ol class="carousel-idicators">
+							<?php
+							include("connection.php");
+							$a=0;
+							$query='SELECT * FROM samples';
+							$sql=mysqli_query($con,$query);
+							while($row=mysqli_fetch_array($sql))
+							{
+								?>
+								<li data-target="#mycarousel" data-slide-to="<?php echo $a++;?>">
+								 
+								</li>
+								<?php
+								
+							}
+							
+							?>
+							</ol>
+							
+							
+							<div class="carousel-inner" role="listbox">
+							<?php
+								$query="SELECT * FROM samples";
+								$sqli=mysqli_query($con,$queryy);
+								while($img=mysqli_fetch_array($sqli))
+								{
+									?>
+									<div class="item">
+									<img src="proimages/<?php echo $img['title'];?>"
+									class="img resposive" alt="<?php $img['title'];?>">
+									
+									</div>
+									<?php
+								}
+							?>
+						</div>
+						<a href="left carousel-control" href="#mycarousel" role="button" data-slide="next">
+						</a>
+						<a href="right carousel-control" href="#mycarousel" role="button" data-slide="next">
+						</a>
+					</div>
 
-</div>
+		</div>	
+
+		<script type="text/javascript">
+		$(document).ready(function(e)
+		{
+			$('.carousel-indicators li:nt-child(1)').addClass('active');
+			$('.item:nth-child(1)').addClass()
+			});
+		</script>
+		
+</body>
+</html>
